1ce6ccc4de6aa12a85bfda8f53e4741cbea47525
Hethsron <hethsronboueya@gmail.com>
2021-01-10 13:18:50 +0100

    add User model

---
+++ src/main/java/fr/uha/ensisa/aia/model/User.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright © 2020  	Hethsron Jedaël BOUEYA
+ * 						Omar CHICHAOUI
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package fr.uha.ensisa.aia.model;
+/**
+ *		@file            	User.java
+ *      @details
+ *
+ *      @author          	Hethsron Jedaël BOUEYA (hethsron-jedael.boueya@uha.fr)
+ *      					Omar CHICHAOUI (omar.chichaoui@uha.fr)
+ *
+ *      @version         	0.0.1
+ *      @date            	December, 9th 2020
+ *
+ *      @Copyright       	GPLv3+ : GNU GPL version 3 or later
+ *                       	Licencied Material - Property of Us®
+ *                       	© 2020 ENSISA (UHA) - All rights reserved.
+ */
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * @class       User
+ * @brief       Implements the domain model
+ */
+public class User {
+
+    private long id;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String password;
+    private String date;
+
+    public User(String lastname, String firstname, String email, String password) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+        this.password = password;
+        this.date = LocalDateTime.now().toString();
+        this.id = lastname.hashCode() + firstname.hashCode() + email.hashCode() + password.hashCode() + date.hashCode();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return id == user.id && lastname.equals(user.lastname) && firstname.equals(user.firstname) && email.equals(user.email) && password.equals(user.password) && date.equals(user.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lastname, firstname, email, password, date);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
